7b79d3e57dcde8529c66b077b64a74453453f772
Name <audtjs4055@naver.com>
2022-02-15 17:43:56 +0900

    ss

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 var express = require("express");
+
+
 var app = express();
 app.use("/test",express.static("sample"));
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-});
+ app.get("/",(req,res)=>{
+     res.send("Hello World");
+ });
 
 app.get("/hi",(req,res)=>{
     res.send("Hi");
+});
+
+app.get("/add",(req,res)=>{
+    var p1 =req.param('p1');
+    var p2 =req.param('p2');
+    var result = Number(p1)+Number(p2);
+    res.send("p1+p2="+result);
+});
+
+app.get("/minus",(req,res)=>{
+    var p1 =req.param('p1');
+    var p2 =req.param('p2');
+    var result = Number(p1)-Number(p2);
+    res.send("p1+p2="+result);
+});
+
+app.get("/multiply",(req,res)=>{
+    var p1 =req.param('p1');
+    var p2 =req.param('p2');
+    var result = Number(p1)*Number(p2);
+    res.send("p1+p2="+result);
+});
+
+app.get("/divide",(req,res)=>{
+    var p1 =req.param('p1');
+    var p2 =req.param('p2');
+    var result = Number(p1)/Number(p2);
+    res.send("p1+p2="+result);
 });
 
 app.post("/",(req,res)=>{
